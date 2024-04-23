61522a052c11338fd8bd531666f584a71989d5d6
hyunjoon park <105212959+hyeonjoonpark@users.noreply.github.com>
2024-04-23 19:30:54 +0900

    FEAT: grow 함수

--- provider_ex_02/lib/components/dog_widget.dart
+++ provider_ex_02/lib/components/dog_widget.dart
@@ -33,6 +33,14 @@ class Age extends StatelessWidget {
           '- age : ${Provider.of<Dog>(context).age}',
           style: TextStyle(fontSize: 20),
         ),
+        SizedBox(height: 20),
+        ElevatedButton(
+          onPressed: () {
+            // Provider.of 함수의 context를 사용하여 Dog 객체의 grow 메서드 호출
+            Provider.of<Dog>(context, listen: false).grow();
+          },
+          child: Text('Grow'),
+        ),
       ],
     );
   }
